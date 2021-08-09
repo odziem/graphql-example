79fd998ec9a9eac2c161c451be44a08fbf6043a6
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-08-09 12:40:35 -0400

    Add schema and sample data

--- server.js
+++ server.js
@@ -4,14 +4,63 @@ const { graphqlHTTP } = require('express-graphql');
 
 const schema = buildSchema(`
   type Query {
-    description: String
-    price: Float
+    products: [Product]
+    orders: [Order]
+  }
+
+  type Product {
+    id: ID!
+    description: String!
+    reviews: [Review]
+    price: Float!
+  }
+
+  type Review {
+    rating: Int!
+    comment: String
+  }
+
+  type Order {
+    date: String!
+    subtotal: Float!
+    items: [OrderItem]
+  }
+
+  type OrderItem {
+    product: Product!
+    quantity: Int!
   }
 `);
 
 const root = {
-  description: 'Red Shoe',
-  price: 42.12,
+  products: [
+    {
+      id: 'redshoe',
+      description: 'Red Shoe',
+      price: 42.12,
+    },
+    {
+      id: 'bluejean',
+      description: 'Blue Jeans',
+      price: 55.55,
+    }
+  ],
+  orders: [
+    {
+      date: '2005-05-05',
+      subtotal: 90.22,
+      items: [ 
+        {
+          product: {
+            id: 'redshoe',
+            description: 'Old Red Shoe',
+            price: 45.11,
+          },
+          quantity: 2,
+        }
+      ]
+    }
+  ]
 };
 
 const app = express();
