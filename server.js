b1724c90e11fd95e90dc19f228e5ae7dfe06aaff
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-08-09 12:40:35 -0400

    Add schema and sample data

--- server.js
+++ server.js
@@ -4,8 +4,31 @@ const { createYoga } = require('graphql-yoga');
 
 const schema = buildSchema(`
   type Query {
-    description: String
-    price: Float
+    products: [Product]
+    orders: [Order]
+  }
+
+  type Product {
+    id: ID!
+    description: String!
+    reviews: [Review]
+    price: Float!
+  }
+
+  type Review {
+    rating: Int!
+    comment: String
+  }
+
+  type Order {
+    date: String!
+    subtotal: Float!
+    items: [OrderItem]
+  }
+
+  type OrderItem {
+    product: Product!
+    quantity: Int!
   }
 `);
 
