581b7b6c58558643825f097cdd7ebcd0d540f279
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-08-11 15:45:56 -0400

    Modularize schemas using graphql-tools

--- server.js
+++ server.js
@@ -1,66 +1,20 @@
+const path = require('path');
 const express = require('express');
-const { buildSchema } = require('graphql');
+
 const { graphqlHTTP } = require('express-graphql');
 
-const schema = buildSchema(`
-  type Query {
-    products: [Product]
-    orders: [Order]
-  }
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
-  type Product {
-    id: ID!
-    description: String!
-    reviews: [Review]
-    price: Float!
-  }
+const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
 
-  type Review {
-    rating: Int!
-    comment: String
-  }
-
-  type Order {
-    date: String!
-    subtotal: Float!
-    items: [OrderItem]
-  }
-
-  type OrderItem {
-    product: Product!
-    quantity: Int!
-  }
-`);
+const schema = makeExecutableSchema({
+  typeDefs: typesArray
+});
 
 const root = {
-  products: [
-    {
-      id: 'redshoe',
-      description: 'Red Shoe',
-      price: 42.12,
-    },
-    {
-      id: 'bluejean',
-      description: 'Blue Jeans',
-      price: 55.55,
-    }
-  ],
-  orders: [
-    {
-      date: '2005-05-05',
-      subtotal: 90.22,
-      items: [ 
-        {
-          product: {
-            id: 'redshoe',
-            description: 'Old Red Shoe',
-            price: 45.11,
-          },
-          quantity: 2,
-        }
-      ]
-    }
-  ]
+  products: require('./products/products.model'),
+  orders: require('./orders/orders.model'),
 };
 
 const app = express();
