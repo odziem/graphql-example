c8bc0086d1f0b4f15db1cfe01a604c1b05f8c2ef
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-08-21 10:04:39 -0400

    Modularized resolvers and better models

--- orders/orders.model.js
+++ orders/orders.model.js
@@ -1,4 +1,4 @@
-module.exports = [
+const orders = [
   {
     date: '2005-05-05',
     subtotal: 90.22,
@@ -14,3 +14,11 @@ module.exports = [
     ]
   }
 ];
+
+function getAllOrders() {
+  return orders;
+}
+
+module.exports = {
+  getAllOrders,
+};
--- products/products.model.js
+++ products/products.model.js
@@ -1,4 +1,4 @@
-module.exports = [
+const products = [
   {
     id: 'redshoe',
     description: 'Red Shoe',
@@ -10,3 +10,11 @@ module.exports = [
     price: 55.55,
   }
 ];
+
+function getAllProducts() {
+  return products;
+}
+
+module.exports = {
+  getAllProducts,
+};
