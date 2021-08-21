2e637379a6eaed7b98fd28ac365e3474bd859ef0
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-08-21 15:42:15 -0400

    Get product by ID

--- products/products.model.js
+++ products/products.model.js
@@ -18,10 +18,17 @@ function getAllProducts() {
 function getProductsByPrice(min, max) {
   return products.filter((product) => {
     return product.price >= min && product.price <= max;
-  })
+  });
+}
+
+function getProductById(id) {
+  return products.find((product) => {
+    return product.id === id;
+  });
 }
 
 module.exports = {
   getAllProducts,
   getProductsByPrice,
+  getProductById,
 };
--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -7,6 +7,9 @@ module.exports = {
     },
     productsByPrice: (_, args) => {
       return productsModel.getProductsByPrice(args.min, args.max);
+    },
+    product: (_, args) => {
+      return productsModel.getProductById(args.id);
     }
   }
 };
