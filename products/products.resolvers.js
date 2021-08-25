6d8accc98433251c5cc9dc54ee06becf2d55fbfd
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-08-25 04:01:37 -0400

    Add new product mutation

--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -11,5 +11,10 @@ module.exports = {
     product: (_, args) => {
       return productsModel.getProductById(args.id);
     }
+  },
+  Mutation: {
+    addNewProduct: (_, args) => {
+      return productsModel.addNewProduct(args.id, args.description, args.price);
+    }
   }
 };
